e5d73bfdbfa1a7dccdcdfbda2800771fa621201b
Evgeniia Patrikeeva <sunny.zhe@mail.ru>
2024-10-02 01:00:24 +0500

    new logic for task 897

--- src/main/java/ru/stepUp/Inno_Qa/MainApp.java
+++ src/main/java/ru/stepUp/Inno_Qa/MainApp.java
@@ -4,5 +4,7 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println(12);
         System.out.println(12);
+        System.out.println(12);
+        System.out.println(12);
     }
 }
